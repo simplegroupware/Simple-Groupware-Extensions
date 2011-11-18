41a283992858448afd87a03dd39e2b6785ad1aa0
Thomas Bley <thbley+github@gmail.com>
2011-11-18 03:30:09 +0100

    added new htmlfield function to filter html content, without norefer

--- compose_html/core/classes/compose_html.php
+++ compose_html/core/classes/compose_html.php
@@ -29,4 +29,33 @@ static function createemail($id, $data) {
   return "";
 }
 
+static function htmlfield($text) {
+  $badtags = array("applet","area","base","body","button","embed","form","frame","frameset","head",
+    "html","input","link","map","meta","object","script","select","style","textarea","title","!doctype","iframe");
+  $text = str_replace(chr(0),"",$text); // remove null-byte vulnerability
+  $text = preg_replace(array("|<head[^>]*?>.*?</head>|si","|<script[^>]*?>.*?</script>|si","|<style[^>]*?>.*?</style>|si","|<!--.*?-->|si"),"",$text);
+  $text = explode("<"," ".$text);
+  $result = array_shift($text);
+  foreach ($text as $v) {
+    $v = explode(">",$v);
+    $bad_tag = false;
+	$tag_new = "";
+    $tag_arr = explode(" ",trim(str_replace(array("\t","(")," ",$v[0])));
+	foreach ($tag_arr as $key=>$item_str) {
+	  $item = explode("=", $item_str, 2);
+	  $item[0] = strtolower($item[0]);
+	  if (strlen($item[0])!=0 and $item[0][0]=="/") $item[0] = substr($item[0],1);
+	  if (preg_match("/^(\?|on|mce|background\$)/",$item[0])) continue;
+	  if (!empty($item[1]) and stripos($item[1],"javascript:")) continue;
+	  if ($key==0 and in_array($item[0],$badtags)) {
+	    $bad_tag = true;
+		break;
+	  } else $tag_new .= " ".$item_str;
+	}
+	if (!$bad_tag) $result .= "<".trim($tag_new).">";
+	if (!empty($v[1])) $result .= $v[1];
+  }
+  return $result;
+}
+
 }
